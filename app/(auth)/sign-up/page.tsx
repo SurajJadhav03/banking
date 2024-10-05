69260634ae65dec372d5e28dcd51ee9b7675b865
SurajJadhav03 <sj555182@gmail.com>
2024-10-05 18:55:04 +0530

    Sign In/Up functionality implemented

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUP = () => {
+const SignUP = async () => {
   return (
-    <div>SignUP</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type='sign-up'/>
+    </section>
   )
 }
 
